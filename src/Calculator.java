1634d05a8071bee6963d0e4648a07f869b5f3588
Krzysztof <Krzysztof@kursGIT.pl>
2020-12-03 12:19:54 +0000

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(3,2));
+        System.out.println(substract(3,2));
+        System.out.println(multiplication(3,2));
+        System.out.println(division(3,2));
+
+
+    }
+
+    private static int sum(int x, int y) {
+        return x + y;
+    }
+
+    private static int substract(int x, int y) {
+        return x - y;
+    }
+
+    private static int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    private static int division(int x, int y) {
+        return x / y;
+    }
+}
